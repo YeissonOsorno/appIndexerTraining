ad3b0cbf8e5eb6e6d8f7d6536c00c900253f4db7
ale.miranda <ale.miranda.barrera@gmail.com>
2020-11-03 07:55:44 -0500

    pull request - changing all code in the themes, creation of articles with infomative and didactive

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+/*
 var firebaseConfig = {
     apiKey: "AIzaSyDnBnQ4DKgcwuQDmAORVDn8xFODZoTj4C4",
     authDomain: "kick-off-2ad9c.firebaseapp.com",
@@ -12,4 +13,20 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   const fb = firebase.initializeApp(firebaseConfig);
-  export  const database = fb.firestore();
+  export  const database = fb.firestore(); 
+
+*/
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyD_IWT3G45OxBdmh2yrqldW3qTqfF-z4EI",
+    authDomain: "indexer-training-db.firebaseapp.com",
+    databaseURL: "https://indexer-training-db.firebaseio.com",
+    projectId: "indexer-training-db",
+    storageBucket: "indexer-training-db.appspot.com",
+    messagingSenderId: "916365182729",
+    appId: "1:916365182729:web:b9a2a153a2e32463bbf5d8",
+    measurementId: "G-6NJL76WJNL"
+  };
+  // Initialize Firebase
+  const fb = firebase.initializeApp(firebaseConfig);
+  export  const database = fb.firestore();  
--- src/modules/Themes/Themes.js
+++ src/modules/Themes/Themes.js
@@ -1,31 +1,70 @@
 import React ,{ useState } from 'react';
-
 import Informative from '../../modules/Themes/Informative/Informative';
 import Didactic from '../../modules/Themes/Didactic/Didactic';
-
 import {database} from '../../config/Firebase';
+
 const Themes = () => {
-    const [data, setData] = useState({ informative:"", didactic:"",titulo:""});
-    const [questions, setQuestion] = useState([]);
+
+    const inicialStateValues = {title: "", author: "", article: [] }
+    const inicialStateInformative= {title:"" ,textArea:"", img: "", url: ""}
+    const inicialStateDidactic= {
+            question: "",            
+            options:{
+                opt1:"",
+                opt2:"",
+                opt3:"",
+                opt4:""
+                },
+            correct_anwswer: ""
+    }        
+    const [data, setData] = useState(inicialStateValues);
+    const [informative, setInformative] = useState(inicialStateInformative);
+    const [didactic, setDidactic] = useState(inicialStateDidactic);
+
+    const handleOnchangeInformative =(e)=>{
+        setInformative({
+            ...informative,
+            [e.target.name]:e.target.value
+        })
+    } 
+
+    const handleOnchangeOptions =(e)=>{        
+        setDidactic({   
+            ...didactic,                     
+            options: {...didactic.options, [e.target.name]: e.target.value}
+        })        
+    } 
+    
+    const handleOnchangeDidatic =(e)=>{
+        setDidactic({
+            ...didactic,
+            [e.target.name]:e.target.value            
+        })
+    }
 
     const handleOnchange =(e)=>{
         setData({
             ...data,
             [e.target.name]:e.target.value
         })
-
     }
 
     const handleSubmit =()=>{
-        setQuestion([...questions,{question:data.didactic,category:"didactic"}]);
-        
+        const article = {informative, didactic};
+        setData({
+            ...data,
+            article: [...data.article, article] 
+        });
+        setInformative(inicialStateInformative);
+        setDidactic(inicialStateDidactic);
     }
-     const handleSave = async ()=>{
-        const themeData = {data,questions};
-        await database.collection('themes').doc().set(themeData);
-        setData({ informative:"", didactic:"",titulo:""});
-        setQuestion([])
-     }
+
+    const handleSave = async ()=>{        
+        await database.collection('themes').doc().set(data)
+        setData(inicialStateValues); 
+        setInformative(inicialStateInformative);
+        setDidactic(inicialStateDidactic);
+    }
     return ( 
         <>
             
@@ -35,18 +74,18 @@ const Themes = () => {
                     <input 
                         onChange={(e)=>handleOnchange(e)} 
                         type="text" 
-                        name="titulo" 
+                        name="title" 
                         placeholder="Crea el titulo" 
-                        value= {data.titulo}    
+                        value= {data.titulo}
                     />
                 </div>
                 <div className="row">
-                    <Informative onchange={handleOnchange}></Informative>
+                    <Informative handleOnchange={handleOnchangeInformative}></Informative>
                 </div>
                 <div className="row">
-                    <Didactic onchange={handleOnchange}></Didactic>
+                    <Didactic handleOnchange={handleOnchangeDidatic} handleOptions={handleOnchangeOptions}></Didactic>
                 </div>
-                <button onClick={handleSubmit}>Guardar y crear otra pregunta</button>
+                <button onClick={handleSubmit}>Guardar y crear otro articulo</button>
                 <button onClick={handleSave}>Subir a database</button>
             </div>
 
